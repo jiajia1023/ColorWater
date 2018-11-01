e4980574b1af55c42d8d4be51c81aa5b5f95210b
jiajia1023 <328453479@qq.com>
2018-11-01 17:07:58 +0800

    测试autobanner

--- app/src/main/java/com/colorwater/main/HomeFragment.kt
+++ app/src/main/java/com/colorwater/main/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.colorwater.main
 
 import android.view.View
+import android.widget.RelativeLayout
 import android.widget.Toast
 import com.colorwater.R
 import com.morelibrary.ui.BaseFragment
@@ -21,6 +22,9 @@ class HomeFragment : BaseFragment() {
     }
 
     override fun onBindData() {
+        fragmentHome_banner!!.setBannerWidthAndHeight(RelativeLayout.LayoutParams.MATCH_PARENT,600)
+        fragmentHome_banner!!.onSetMode_BottomParent(RelativeLayout.ALIGN_PARENT_RIGHT)
+//        fragmentHome_banner!!.onSetRadioGroupLayouParams()
         var urlList = ArrayList<String>()
         urlList.add("http://img4.duitang.com/uploads/item/201601/12/20160112201847_dTscn.jpeg")
         urlList.add("http://pic36.photophoto.cn/20150728/0022005597823716_b.jpg")
